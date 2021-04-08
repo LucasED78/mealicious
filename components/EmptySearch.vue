b1b35b0292816d53e5b2d7c213118697b7b0f07c
Lucas Eduardo <lucased78@hotmail.com>
2021-04-08 09:35:12 -0300

    feat: adding EmptySearch component

---
+++ components/EmptySearch.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex justify-center mb-10">
+    <div>
+      <h1 class="text-2xl mb-2 font-bold text-center">
+        We haven't found anything that matches your search :(
+      </h1>
+
+      <ul>
+        <li>
+          Review your ortography
+        </li>
+
+        <li class="my-2">
+          Use less or more generic words
+        </li>
+
+        <li>
+          If you still stuck, try our suggestions:
+
+          <a
+            v-for="(suggestion, index) in suggestions"
+            :key="`${suggestion}-${index}`"
+            class="text-indigo-500"
+            href="#"
+            @click="$emit('click', suggestion)"
+          >{{ suggestion }}, </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+export default Vue.extend({
+  props: {
+    suggestions: {
+      type: Array as PropType<Array<string>>,
+      default: () => [
+        'tomatoes', 'bread', 'chicken'
+      ]
+    }
+  }
+})
+</script>
