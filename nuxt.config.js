110b91206829a9f5bba39be2b2729749e9ffbea8
Lucas Eduardo <lucased78@hotmail.com>
2021-04-08 17:32:04 -0300

    chore: adding favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'meals_app',
+    title: 'Mealicious',
     htmlAttrs: {
       lang: 'en'
     },
