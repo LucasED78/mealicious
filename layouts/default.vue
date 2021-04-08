058a010e021b8e949b41be1ba06da316fde94d42
Lucas Eduardo <lucased78@hotmail.com>
2021-04-08 17:16:50 -0300

    feat: adding font

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,7 @@
 <style>
 html {
   font-family:
+    'Lato',
     'Source Sans Pro',
     -apple-system,
     BlinkMacSystemFont,
@@ -31,33 +32,8 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+body.no-scroll {
+  overflow: hidden;
 }
 
 .default-enter-active, .default-leave-active {
--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap' }
     ]
   },
 
